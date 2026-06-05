ae5b954ae3407570ce998d07eabcc42f1bc307c3
김정현 <shshjang14@gmail.com>
2026-06-06 01:34:48 +0900

    최종피드백-member-only 클래스명 통일

--- src/script/main.js
+++ src/script/main.js
@@ -73,13 +73,13 @@ window.addEventListener('DOMContentLoaded', () => {
       logoutEl.innerText = ''
     }
   }
-  // mypage-link 표시 토글
-  const mypageLinks = document.querySelectorAll('.mypage-link')
-  if (mypageLinks.length) {
-    mypageLinks.forEach((el) => {
-      el.style.display = user ? '' : 'none'
-    })
-  }
+  // // mypage-link 표시 토글
+  // const mypageLinks = document.querySelectorAll('.mypage-link')
+  // if (mypageLinks.length) {
+  //   mypageLinks.forEach((el) => {
+  //     el.style.display = user ? '' : 'none'
+  //   })
+  // }
   // 멤버 전용 네비게이션 링크
   const memberLinks = document.querySelectorAll('.member-only')
   if (memberLinks.length) {
